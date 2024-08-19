8449a34a0caee799856058cdae2ea5039ab51096
Mahyar <mahyaruchiha@gmail.com>
2024-08-19 03:23:59 -0700

    fixing small issue

--- sound.py
+++ sound.py
@@ -1,11 +1,13 @@
 import pygame
+import pygame.mixer
 
 
 class Sound:
     """Initialize the sound effect and background music of the game"""
     def __init__(self):
         self.bg_music = pygame.mixer.music.load("bg_music.mp3")
-        self.bullet_sound = pygame.mixer.Sound("bullet_sound.mp3")
+        self.bullet_sound = pygame.mixer.Sound("bullet_sound_1.wav")
+        self.bullet_sound.set_volume(0.25)
 
     def fire_sound(self):
         self.bullet_sound.play()
