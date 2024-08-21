7d3c335f86dbfcd53e7e3770cf84eea1e8cd75bf
MahyarPiade <underratednoobprogrammer@gmail.com>
2024-08-21 00:38:14 -0700

    space

--- sound.py
+++ sound.py
@@ -6,7 +6,7 @@ class Sound:
     """Initialize the sound effect and background music of the game"""
     def __init__(self):
         self.bg_music = pygame.mixer.music.load("bg_music.mp3")
-        pygame.mixer.music.set_volume(0.75)
+        pygame.mixer.music.set_volume(1)
 
         self.bullet_sound = pygame.mixer.Sound("bullet_sound_1.wav")
         self.bullet_sound.set_volume(0.25)
